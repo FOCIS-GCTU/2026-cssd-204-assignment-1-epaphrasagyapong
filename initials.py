cb9136aca3cf08511697adf217db8b18211429ff
Agyapong Epaphras Daleku Mawufemor <157484381+epaphrasagyapong@users.noreply.github.com>
2026-05-30 23:45:35 +0000

    function to print my initials

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Prints out my initials.
+# Assignment Number: 1
+#
+# Name: Epaphras Agyapong Daleku Mawufemor
+# STUDENT ID:2425401280
+# Email: 2425401280@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor,Epaphras Agyapong Daleku Mawufemor, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This function prints the initials EAD in large stylized formats.
+    print()
+    print("...EAD")
+    print()
+    print("...EEEEEEEEEEEE............AAAA............DDDDDDDDDD.....")
+    print("...EEEEEEEEEEEE...........AA..AA...........DDDDDDDDDDD....")
+    print("...EE....................AA....AA..........DD.......DD....")
+    print("...EE...................AA......AA ........DD........DD...")
+    print("...EEEEEEEE............AAAAAAAAAAAA........DD........DD...")
+    print("...EEEEEEEE............AAAAAAAAAAAA........DD........DD...")
+    print("...EE..................AA........AA........DD........DD...")
+    print("...EE..................AA........AA........DD.......DD....")
+    print("...EEEEEEEEEEEE...**...AA........AA...**...DDDDDDDDDDD....**")
+    print("...EEEEEEEEEEEE...**...AA........AA...**...DDDDDDDDDD.....**")
+    print()
 
 
 main()
